fa3accfa8371423d344916d91a0d42226dd0bf8f
Oyewale Emmanuel <oyewaleadeyemi3@gmail.com>
2023-01-31 15:15:54 -0800

    start project

---
+++ brown.js
@@ -0,0 +1,29 @@
+Javascript---
+--Declaration
+variable var a = 5;
+constant const a = 5;
+
+--Events
+onclick
+onmouseover etc.
+
+document.getElementBy
+id('')
+
+---------
+
+if/else
+nested if
+
+--if/else syntax------
+(1) &&   (2)||
+if(a=5) {
+	document.alert('Emmanuel is stubborn')
+} else {
+	document.alert('Emmanuel is a good boy')
+}
+
+function()
+
+Username : user
+
---
+++ hello.js
@@ -0,0 +1,6 @@
+*{
+	function funky() {
+		// body...
+		alert("hi world")
+	}
+}
